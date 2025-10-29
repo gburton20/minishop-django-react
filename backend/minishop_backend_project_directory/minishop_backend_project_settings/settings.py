2080cfef2ac25abbc37908f05df6e1b375930f74
gburton20 <64306503+gburton20@users.noreply.github.com>
2025-10-29 09:30:19 -0400

    Update database settings to support PostgreSQL and add migration files

--- backend/minishop_backend_project_directory/minishop_backend_project_settings/settings.py
+++ backend/minishop_backend_project_directory/minishop_backend_project_settings/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,13 +78,21 @@ WSGI_APPLICATION = 'minishop_backend_project_settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+# Check if DATABASE_URL is set (production with Supabase)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    # dj_database_url.parse returns a Django DATABASES configuration dictionary for a single DB:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
     }
-}
-
+else:
+    # Default SQLite config for local development:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
