26bd22f8c472be63b725379d5bcc29cd5a366c50
gburton20 <64306503+gburton20@users.noreply.github.com>
2026-02-10 17:04:43 -0500

    Progress made toward one-line layout of all components at each breakpoint, with <SearchInput/> commanding more width as viewport width increases

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,90 +12,98 @@ const Navbar = ({navCartAddCount}) => {
   const hideSearch = location.pathname.startsWith('/cart');
 
   return (
-    <>
-      <div className={`navbar flex flex-col p-2 gap-2 
+    <div className={`navbar flex flex-col p-2 gap-2 w-screen 
+    
+    sm:flex-row sm:items-center sm:gap-1 sm:p-1 sm:pt-4 sm:justify-between sm:w-full sm:pl-2 sm:pr-2
+
+    ${
+      hideSearch
+      ? 'justify-around'
+      : ''
+    }
+    
+    `}>
+
+      <div className={`logo-text-and-log-in-button-section 
       
-      sm:flex-row sm:items-center sm:gap-1 sm:p-1
+      order-0 row-1 flex justify-around w-full
 
       ${
-        hideSearch
-        ? 'justify-around'
-        : ''
-      }
+            isAuthenticated 
+            ? 'justify-center'
+            : ''
+          }
       
+      sm:contents
+
       `}>
-        <div className={`logo-text-and-log-in-button-section order-0 row-1 flex flex-wrap justify-around
-
-        ${
-              isAuthenticated 
-              ? 'justify-center'
-              : ''
-            }
         
-        sm:order-0 sm:flex-nowrap
-
-        `}>
+        {/* Minishop thumbnail logo: */}
+        <Link to="/" className="order-0 sm:order-0">
+          <div className="thumbnail-and-text-div flex order-first
           
-          <div className={`logo-section flex pt-2`}>
-            {/* Minishop thumbnail logo: */}
-            <Link to="/">
-              <img 
-                className='logo w-[40vw] h-[20vw] align-middle
-                
-                sm:w-[9vw] sm:h-[8vh] relative bottom-2
-
-                md:w-[18vw] md:h-[10vh]
-                
-                '
-                src="/entire_minishop_logo_(with text).svg"
+          sm:relative sm:bottom-2.5
+          ">
+              <img
+                className="w-14 h-18 object-contain" 
+                src="/minishop_logo.svg"
                 alt="Minishop logo"
-                />
-            </Link>
+              />
+              {/* 'inishop' text */}
+              <h1
+                className='relative text-4xl font-bold top-6'
+                >
+                <span className='inline-block bg-[linear-gradient(135deg,#d4bb4f_0%,#5f8f3b_100%)] bg-clip-text text-transparent'>ini</span>shop
+              </h1>
           </div>
+        </Link>
 
-          {/* 'Log in' <AuthButton component */}
-          <div className="relative grow-0 shrink-0 text-center top-9 flex-none
-          
-          sm:order-2 sm:grow-0 sm:shrink sm:basis-auto sm:text-left
-          
-          "
-          >
-            {!isAuthenticated && (
-              <LoginButton/> 
-            )}
-          </div>
-        </div>
-
-        {/* 'Log out' <AuthButton component */}
-        <div className="order-2 row-2 
+        {/* 'Log in' <AuthButton component */}
+        <div className="relative grow-0 shrink-0 text-center top-6 order-1
         
-        sm:order-2
+        sm:relative sm:top-px sm:grow-0 sm:shrink sm:basis-auto sm:text-left sm:order-3
         
         ">
-          {isAuthenticated && (
-            <AuthButton 
-            navCartAddCount={navCartAddCount}
-            />
+          {!isAuthenticated && (
+            <LoginButton/> 
           )}
         </div>
+        
+      </div> 
 
-        {!hideSearch && (
-          <div className="order-3 grow-0 shrink-0 basis-full w-full py-2 
-          
-          sm:order-1 sm:grow-0 sm:shrink sm:basis-auto sm:w-[45%]
-
-
-          ">
-            <SearchInput />
-          </div>
+      
+      {/* 'Log out' <AuthButton component */}
+      <div className="order-2 row-2 
+      
+      sm:order-3
+      
+      ">
+        {isAuthenticated && (
+          <AuthButton 
+          navCartAddCount={navCartAddCount}
+          />
         )}
-
-        {/* CartNavIcon: hidden on mobile, visible on sm+ */}
-        <Link to="/cart" className="order-4 flex-none hidden sm:block sm:order-1">
-          <CartNavIcon navCartAddCount={navCartAddCount} />
-        </Link>
       </div>
-    </>
+
+      {!hideSearch && (
+        <div className="order-3 grow-0 shrink-0 basis-full w-full py-2 
+        
+        sm:order-1 sm:grow-0 sm:shrink sm:basis-auto sm:w-[45%] lg:w-[35%]
+
+        ">
+          <SearchInput />
+        </div>
+      )}
+
+      {/* CartNavIcon: hidden on mobile, visible on sm+ */}
+      <Link to="/cart" className="order-4 flex-none hidden 
+      
+      sm:block sm:order-2
+      
+      ">
+        <CartNavIcon navCartAddCount={navCartAddCount} />
+      </Link>
+    </div>
   )
 }
 
