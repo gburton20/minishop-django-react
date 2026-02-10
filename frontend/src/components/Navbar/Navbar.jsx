4996e564fb347889740e19a5fa087742531829d0
gburton20 <64306503+gburton20@users.noreply.github.com>
2026-02-10 14:46:41 -0500

    Decided to redesign the Minishop logo in Figma, by incorporating text into the imported .svg file. Propose reverting to retaining text within the component .jsx file.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -13,45 +13,50 @@ const Navbar = ({navCartAddCount}) => {
 
   return (
     <>
-      <div className='navbar flex flex-col p-2 gap-2 
+      <div className={`navbar flex flex-col p-2 gap-2 
       
-      sm:flex-row
+      sm:flex-row sm:items-center sm:gap-1 sm:p-1
+
+      ${
+        hideSearch
+        ? 'justify-around'
+        : ''
+      }
       
-      '>
-        <div className={`logo-text-and-log-in-button-section order-1 row-1 flex flex-wrap justify-around
+      `}>
+        <div className={`logo-text-and-log-in-button-section order-0 row-1 flex flex-wrap justify-around
+
         ${
               isAuthenticated 
               ? 'justify-center'
               : ''
             }
         
+        sm:order-0 sm:flex-nowrap
+
         `}>
           
-          <div className={`logo-and-text-section flex
-            
-          
-          `}>
+          <div className={`logo-section flex pt-2`}>
             {/* Minishop thumbnail logo: */}
             <Link to="/">
               <img 
-                className='logo w-[18vw] h-[10vh] align-middle'
-                src="/minishop_logo.svg"
+                className='logo w-[40vw] h-[20vw] align-middle
+                
+                sm:w-[9vw] sm:h-[8vh] relative bottom-2
+
+                md:w-[18vw] md:h-[10vh]
+                
+                '
+                src="/entire_minishop_logo_(with text).svg"
                 alt="Minishop logo"
                 />
             </Link>
-
-            {/* 'inishop' text */}
-            <h1
-              className='relative top-8 text-4xl font-bold text-right'
-              >
-              <span className='inline-block bg-[linear-gradient(135deg,#d4bb4f_0%,#5f8f3b_100%)] bg-clip-text text-transparent'>ini</span>shop
-            </h1>
           </div>
 
-          {/* Logged OUT <AuthButton component */}
+          {/* 'Log in' <AuthButton component */}
           <div className="relative grow-0 shrink-0 text-center top-9 flex-none
           
-          sm:order-0 sm:grow-0 sm:shrink sm:basis-auto sm:text-left
+          sm:order-2 sm:grow-0 sm:shrink sm:basis-auto sm:text-left
           
           "
           >
@@ -61,8 +66,12 @@ const Navbar = ({navCartAddCount}) => {
           </div>
         </div>
 
-        {/* Logged IN <AuthButton component */}
-        <div className="order-2 row-2">
+        {/* 'Log out' <AuthButton component */}
+        <div className="order-2 row-2 
+        
+        sm:order-2
+        
+        ">
           {isAuthenticated && (
             <AuthButton 
             navCartAddCount={navCartAddCount}
@@ -70,18 +79,19 @@ const Navbar = ({navCartAddCount}) => {
           )}
         </div>
 
-
-
-
-
         {!hideSearch && (
-          <div className="order-3 grow-0 shrink-0 basis-full w-full py-2 sm:order-0 sm:grow-0 sm:shrink sm:basis-auto">
+          <div className="order-3 grow-0 shrink-0 basis-full w-full py-2 
+          
+          sm:order-1 sm:grow-0 sm:shrink sm:basis-auto sm:w-[45%]
+
+
+          ">
             <SearchInput />
           </div>
         )}
 
         {/* CartNavIcon: hidden on mobile, visible on sm+ */}
-        <Link to="/cart" className="order-4 flex-none hidden sm:block sm:order-0">
+        <Link to="/cart" className="order-4 flex-none hidden sm:block sm:order-1">
           <CartNavIcon navCartAddCount={navCartAddCount} />
         </Link>
       </div>
